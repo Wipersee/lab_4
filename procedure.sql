cbb5d6bc2b993ad2c9cd4c6b304b1d040fbf2692
Wipersee <kesterera@gmail.com>
2020-05-20 22:27:39 +0300

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -1,15 +1,16 @@
 CREATE OR REPLACE procedure license_del(
-    gamer_name  IN  VARCHAR2,
-    game_name IN  VARCHAR2
+    gamer_name  VARCHAR2,
+    game_name VARCHAR2
 )
 IS
 r_contact sales%ROWTYPE;
  BEGIN
    select * INTO r_contact FROM sales
    WHERE sales.name_of_gamer = gamer_name AND sales.name_of_game = game_name;
-   
-   delete r_contact
+   delete  FROM sales
+   WHERE sales.name_of_gamer = gamer_name AND sales.name_of_game = game_name;
    EXCEPTION
     WHEN OTHERS THEN
     dbms_output.put_line('Sorry, this row does not exist !');
 END license_del;
+/
