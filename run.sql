285e672060977dcca4d26f8bd530ddc450bbe982
Wipersee <kesterera@gmail.com>
2020-05-24 12:09:41 +0300

    Update run.sql

--- run.sql
+++ run.sql
@@ -1,14 +1,15 @@
----FIRST OF ALL YOU NEED TO RUN populate.sql and then this file----
+------TEST-----
+---FIRST OF ALL YOU NEED TO RUN populate.sql and package.sql and after that this file----
 
 -----THIS IS THE CHECK FOR PROCEDURE (DBMS OUTPUT MUST BE ENABLE)------
-    exec license_del('Wiperse','Minecraft');
-    exec license_del('Wiperse','Call of Duty 4');
-    exec license_del('JJJJJtest','Minecraft');
-    exec license_del('Wiperse','Minecraft');
+    exec lab_4_proc_func.license_del('Wiperse','Minecraft');
+    exec lab_4_proc_func.license_del('Wiperse','Call of Duty 4');
+    exec lab_4_proc_func.license_del('JJJJJtest','Minecraft');
+    exec lab_4_proc_func.license_del('Wiperse','Minecraft');
     
     
 -----THIS IS THE CHECK FOR FUNCTION (DBMS OUTPUT MUST BE ENABLE)------
-    select * from TABLE(get_sales_for_regions_tf('NA','EU'));
+    select * from TABLE(lab_4_proc_func.get_sales_for_regions_tf('NA','EU'));
     
 
 -----THIS IS THE CHECK FOR TRIGGER (DBMS OUTPUT MUST BE ENABLE)------
